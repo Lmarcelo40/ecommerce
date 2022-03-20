c20f2e4a4194f7d84ebfe4888f18b91ee1dd1b49
Luis Marcelo <lmarcelorfarias@mail.com>
2022-03-19 21:18:59 -0300

    Admin - Produtos

---
+++ admin-categories.php
@@ -0,0 +1,110 @@
+<?php
+
+use \Hcode\PageAdmin;
+use \Hcode\Model\User;
+use \Hcode\Model\Category;
+
+/*---- Rota para acessar o Template Categorias ----*/
+$app->get("/admin/categories", function(){
+
+	User::verifyLogin();
+
+	$categories = Category::listAll();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories", [
+		"categories"=>$categories
+	]);
+
+});
+/*---- Rota Cadastrar ----*/
+$app->get("/admin/categories/create", function(){
+
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories-create");
+
+});
+/*---- Rota para Salvar Categprias ----*/
+$app->post("/admin/categories/create", function(){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->setData($_POST);
+
+	$category->save();
+
+	header("Location: /admin/categories");
+	exit;
+
+});
+
+$app->get("/admin/categories/:idrecovery/delete", function($idcategory){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->delete();
+
+	header("Location: /admin/categories");
+	exit;
+
+});
+/*---- Rota de Edição ----*/
+$app->get("/admin/categories/:idcategory", function($idcategory){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories-update", [
+		"category"=>$category->getValues()
+	]);
+
+});
+
+$app->post("/admin/categories/:idcategory", function($idcategory){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->setData($_POST);
+
+	$category->save();
+
+	header("Location: /admin/categories");
+	exit;
+
+});
+/*---- Rota para Categorias ----*/
+$app->get("/categories/:idcategory", function($idcategory){
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new Page();
+
+	$page->setTpl("category", [
+		'category'=>$category->getValues(),
+		'products'=>[]
+	]);
+
+});
+
+?>
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php 
+/* ROTAS */
 session_start();
 require_once("vendor/autoload.php");
 
@@ -6,58 +7,18 @@ use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
 use \Hcode\Model\User;
+use \Hcode\Model\Category;
+use \Hcode\Model\Product;
 
 $app = new Slim();
 
 $app->config('debug', true);
 
-$app->get('/', function() {
-    
-    $page = new Page();
-
-    $page->setTpl("index");
-
-});
-
-$app->get('/admin', function() {
-   
-	User::verifyLogin();
-
-    $page = new PageAdmin();
-
-    $page->setTpl("index");
-
-});
-
-$app->get('/admin/login', function(){
-
-	$page = new PageAdmin([
-		"header"=>false,
-		"footer"=>false
-	]);
-
-	$page->setTpl("login");
-
-});
-
-$app->post('/admin/login', function(){
-
-	User::login($_POST["login"], $_POST["password"]);
-
-	header("Location: /admin");
-	exit;
-
-});
-
-$app->get('/admin/logout', function(){
-
-	User::logout();
-
-	header("Location: /admin/login");
-	exit;
-
-});
-
+require_once("site.php");
+require_once("admin.php");
+require_once("admin-users.php");
+require_once("admin-categories.php");
+require_once("admin-products.php");
 $app->run();
 
 ?>
